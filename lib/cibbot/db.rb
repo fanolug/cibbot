544ae5a3c87b643bd7251728def9fd247072ca2c
Alessandro Lepore <lepore.ale@gmail.com>
2020-03-14 15:19:35 +0100

    Add DB logging

--- lib/cibbot/db.rb
+++ lib/cibbot/db.rb
@@ -2,5 +2,12 @@
 
 require "dotenv/load"
 require "sequel"
+require_relative "../logging"
+
+include Logging
+
+Sequel::Model.plugin :timestamps
 
 DB = Sequel.connect(ENV["DATABASE_URL"])
+DB.loggers = [logger]
+DB.sql_log_level = :debug
