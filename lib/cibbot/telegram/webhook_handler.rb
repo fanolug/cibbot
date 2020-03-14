26ee02a9616e95b8712a22c91c6484309e597721
Alessandro Lepore <lepore.ale@gmail.com>
2020-03-14 15:20:02 +0100

    Handle /start messages

--- lib/cibbot/telegram/webhook_handler.rb
+++ lib/cibbot/telegram/webhook_handler.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
-require 'json'
-require 'telegram/bot'
+require "json"
+require "telegram/bot"
+require_relative "../models/user"
+require_relative "client"
 
 module Cibbot
   module Telegram
     class WebhookHandler
+      include Cibbot::Telegram::Client
+
       # @param data [JSON] The request data
       def call(data)
-        telegram_message(data)
-        # TODO do something
+        handle_message(telegram_message(data))
       end
 
       private
@@ -18,6 +21,34 @@ module Cibbot
       # @return [Telegram::Bot::Types::Message] The Telegram message
       def telegram_message(data)
         ::Telegram::Bot::Types::Message.new(data["message"])
+      end
+
+      # @param message [Telegram::Bot::Types::Message]
+      def handle_message(message)
+        case message.text
+        when "/start"
+          save_user!(message)
+          send_welcome_message(message)
+        end
+      end
+
+      # @param message [Telegram::Bot::Types::Message]
+      def save_user!(message)
+        user = Cibbot::User.find(chat_id: message.chat.id) ||
+          Cibbot::User.new(chat_id: message.chat.id)
+
+        user.set(
+          username: message.from.username,
+          first_name: message.from.first_name,
+          last_name: message.from.last_name,
+        )
+        user.save
+      end
+
+      # @param message [Telegram::Bot::Types::Message]
+      def send_welcome_message(message)
+        text = "Ciao #{message.from.first_name}, benvenuto!"
+        telegram.send_message(chat_id: message.chat.id, text: text)
       end
     end
   end
